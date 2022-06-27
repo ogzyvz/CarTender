dcc781933a771a499153fdb7f1a39a8a28347553
Ahmet Temiz <ahmet.temiz@infoline-tr.com>
2022-06-27 18:50:05 +0300

    Kategori Modeli yapıldı. Sıkça sorulan sorular sistemi aktif edildi.

---
+++ Infoline.WorkOfTimeManagement/Infoline.WorkOfTimeManagement.BusinessAccess/DatabaseFunctions/Specific/DBVWHDM_Faq.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTimeManagement.BusinessData;
+using System.ComponentModel;
+
+namespace Infoline.WorkOfTimeManagement.BusinessAccess
+{
+    partial class WorkOfTimeManagementDatabase
+    {
+        public VWHDM_Faq[] GetVWHDM_FaqByCategoryId(Guid categoryId, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<VWHDM_Faq>().Where(a => a.categoryId == categoryId).OrderBy(a => a.created).Execute().ToArray();
+            }
+        }
+        public int GetVWHDM_FaqCountAll()
+        {
+            using (var db = GetDB(/*tran*/))
+            {
+                return db.Table<VWHDM_Faq>().Count();
+            }
+        }
+    }
+}
