43702c7232d2a4da995c1403173fd07adfc00305
Ahmet Temiz <ahmet.temiz@infoline-tr.com>
2022-06-28 09:23:10 +0300

    Düzenleme yapıldı

--- Infoline.WorkOfTimeManagement/Infoline.WorkOfTimeManagement.WebProject/Areas/HDM/Controllers/VWHDM_CategoryController.cs
+++ Infoline.WorkOfTimeManagement/Infoline.WorkOfTimeManagement.WebProject/Areas/HDM/Controllers/VWHDM_CategoryController.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Infoline.Framework.Database;
 
 namespace Infoline.WorkOfTimeManagement.WebProject.Areas.HDM.Controllers
 {
